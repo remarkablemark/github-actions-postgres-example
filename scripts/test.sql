cee925d032b6fa8065da39b245cf352ecc9ad224
ichumarinP44 <ilya.chumarin@project44.com>
2022-08-18 23:37:44 -0500

    update script

--- scripts/test.sql
+++ scripts/test.sql
@@ -14,10 +14,3 @@ CREATE TABLE test1(
     created_on TIMESTAMP NOT NULL,
     last_login TIMESTAMP
 );
-CREATE TABLE test2(
-    user_id serial PRIMARY KEY,
-    username VARCHAR (50) UNIQUE NOT NULL,
-    password VARCHAR (50) NOT NULL,
-    email VARCHAR (355) UNIQUE NOT NULL,
-    created_on TIMESTAMP NOT NULL,
-    last_login TIMESTAMP
