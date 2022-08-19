1c24db4fbd99c5c6bc00b1199c209cbbb0130c35
Ilya Chumarin <108356687+ichumarinP44@users.noreply.github.com>
2022-08-18 23:33:24 -0500

    add new script

---
+++ scripts/test.sql
@@ -0,0 +1,23 @@
+CREATE TABLE test0(
+    user_id serial PRIMARY KEY,
+    username VARCHAR (50) UNIQUE NOT NULL,
+    password VARCHAR (50) NOT NULL,
+    email VARCHAR (355) UNIQUE NOT NULL,
+    created_on TIMESTAMP NOT NULL,
+    last_login TIMESTAMP
+);
+CREATE TABLE test1(
+    user_id serial PRIMARY KEY,
+    username VARCHAR (50) UNIQUE NOT NULL,
+    password VARCHAR (50) NOT NULL,
+    email VARCHAR (355) UNIQUE NOT NULL,
+    created_on TIMESTAMP NOT NULL,
+    last_login TIMESTAMP
+);
+CREATE TABLE test2(
+    user_id serial PRIMARY KEY,
+    username VARCHAR (50) UNIQUE NOT NULL,
+    password VARCHAR (50) NOT NULL,
+    email VARCHAR (355) UNIQUE NOT NULL,
+    created_on TIMESTAMP NOT NULL,
+    last_login TIMESTAMP
